135cd256c6bb311262b8db669a4a99b82621790b
Max Webb <webbwrites@gmail.com>
2019-04-03 21:51:50 -0600

    function-conversion.js done

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -3,28 +3,43 @@
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
-// myFunction();
+
+let myFunction = () => {
+  console.log("Function was invoked!");
+};
+
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
-// anotherFunction("Example");
+
+let anotherFunction = (param) => param;
+
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
-// add(1,2);
+
+let add = (param1, param2) => param1 + param2;
+
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
-// subtract(1,2);
+
+let subtract = (param1, param2) => param1 - param2;
+
+subtract(1,2);
 
 
 // Stretch
 
 // exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
+// // const triple = exampleArray.map(function (num) {
+// //   return num * 3;
+// // });
+//
 // console.log(triple);
